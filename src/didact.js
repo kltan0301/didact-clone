852e8c10c0ce810d1db463b5049b5b05a40acbcd
Kai <hades3191@gmail.com>
2023-09-17 21:14:49 +0800

    rendering

--- src/didact.js
+++ src/didact.js
@@ -1,3 +1,79 @@
+let nextUnitOfWork = null;
+let wipRoot = null;
+
+function commitRoot() {
+  commitWork(wipRoot.child)
+  wipRoot = null
+}
+
+function commitWork(fiber) {
+  if (!fiber) { return }
+  const domParent = fiber.parent.dom;
+  domParent.appendChild(fiber.dom)
+  // commitWork(fiber.child)
+  // commitWork(fiber.sibling)
+}
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children;
+  let index = 0
+  let prevSibling = null
+  while(index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    // if first child, assign it as a the fiber's child
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      // if not first child, it becomes a sibling of the previous fiber
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  if (fiber.child) { return fiber.child }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    // Tries to find the uncle node
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+
 function createElement(type, props, ...children) {
   return {
     type,
@@ -22,7 +98,7 @@ function createTextElement(text) {
   }
 }
 
-function render(element, container) {
+function createDom(element) {
   const dom =
     element.type == "TEXT_ELEMENT"
       ? document.createTextNode("")
@@ -34,12 +110,17 @@ function render(element, container) {
     .forEach(name => {
       dom[name] = element.props[name]
     })
+  return dom;
+}
 
-  element.props.children.forEach(child =>
-    render(child, dom)
-  )
-
-  container.appendChild(dom)
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    }
+  }
+  nextUnitOfWork = wipRoot
 }
 
 const Didact = {
@@ -50,8 +131,8 @@ const Didact = {
 const element = Didact.createElement(
   'div',
   { id: 'foo'},
-  Didact.createElement('h1', null, 'bar'),
-  Didact.createElement('b')
+  Didact.createElement('h1', { id: 'heading' }, 'bar'),
+  Didact.createElement('p', { id: 'para' }, 'blah')
 )
 const container = document.getElementById('root');
 
