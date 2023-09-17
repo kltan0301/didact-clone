82ea2f30c5ec5d0a51b8ad14f6d2cd201b6aee76
Kai <hades3191@gmail.com>
2023-09-17 13:32:52 +0800

    didact render

--- src/didact.js
+++ src/didact.js
@@ -3,35 +3,43 @@ function createElement(type, props, ...children) {
     type,
     props: {
       ...props,
-      children: children.map(child => 
-        typeof child === 'object'
+      children: children.map(child =>
+        typeof child === "object"
           ? child
-          : createTextElement(child)  
-      )
-    }
+          : createTextElement(child)
+      ),
+    },
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
   }
 }
 
 function render(element, container) {
-  const dom = element.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(element.type);
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type)
+
   const isProperty = key => key !== "children"
   Object.keys(element.props)
     .filter(isProperty)
     .forEach(name => {
       dom[name] = element.props[name]
     })
-  element.props.children.forEach(child => render(child, dom))
-  container.appendChild(dom);
-}
 
-function createTextElement(text) {
-  return {
-    type: 'TEXT_ELEMENT',
-    props: {
-      nodeValue: text,
-      children: [],
-    }
-  }
+  element.props.children.forEach(child =>
+    render(child, dom)
+  )
+
+  container.appendChild(dom)
 }
 
 const Didact = {
@@ -46,4 +54,5 @@ const element = Didact.createElement(
   Didact.createElement('b')
 )
 const container = document.getElementById('root');
+
 Didact.render(element, container);
